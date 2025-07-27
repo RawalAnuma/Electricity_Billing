80048a99fee0c45b08be1ccd2184fd3224c1ba17
RawalAnuma <rawalanuma@gmail.com>
2025-07-27 23:17:53 +0545

    spring MVC controller that maps URLs to view templates

--- src/main/java/com/demo/billing/controller/ViewController.java
+++ src/main/java/com/demo/billing/controller/ViewController.java
@@ -1,4 +1,29 @@
 package com.demo.billing.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//@RequestMapping("/view")
 public class ViewController {
+    @GetMapping("/home")
+    public String viewHomePage(){
+        return "home";
+    }
+
+    @GetMapping("/calculateBill")
+    public String calculateBillPage() {
+        return "calculateBill";
+    }
+
+    @GetMapping("/showBills")
+    public String viewBillPage() {
+        return "bills";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "about";
+    }
 }

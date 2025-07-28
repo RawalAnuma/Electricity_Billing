77e11512addc87878eb46d8aca8eedf9fcf5f805
RawalAnuma <rawalanuma@gmail.com>
2025-07-28 14:02:38 +0545

    added controller annotation

--- src/main/java/com/demo/billing/controller/BillRecordsController.java
+++ src/main/java/com/demo/billing/controller/BillRecordsController.java
@@ -3,12 +3,13 @@ package com.demo.billing.controller;
 import com.demo.billing.calculation.BillCalculation;
 import com.demo.billing.model.BillRecords;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.demo.billing.service.BillRecordService;
-import java.util.List;
 
-@RestController
+
+@Controller
 @RequestMapping("/billRecords")
 public class BillRecordsController {
 

a4d19a07b9410da5b908aa2552aeb296522d727d
RawalAnuma <rawalanuma@gmail.com>
2025-07-28 13:07:59 +0545

    view for bill calculation added

--- src/main/java/com/demo/billing/controller/ViewController.java
+++ src/main/java/com/demo/billing/controller/ViewController.java
@@ -2,7 +2,7 @@ package com.demo.billing.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 //@RequestMapping("/view")
@@ -10,6 +10,11 @@ public class ViewController {
     @GetMapping("/home")
     public String viewHomePage(){
         return "home";
+    }
+
+    @GetMapping("/customer")
+    public String viewCustomerPage() {
+        return "showCustomers";
     }
 
     @GetMapping("/calculateBill")

d46720490ad83f9abb0a897c34a8281d01010fa6
RawalAnuma <rawalanuma@gmail.com>
2025-07-30 22:20:39 +0545

    updated insertBillRecord method

--- src/main/java/com/demo/billing/service/BillRecordService.java
+++ src/main/java/com/demo/billing/service/BillRecordService.java
@@ -1,6 +1,8 @@
 package com.demo.billing.service;
 
 import com.demo.billing.model.BillRecords;
+import com.demo.billing.model.Customer;
+import com.demo.billing.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.demo.billing.repositories.BillRecordRepository;
@@ -12,9 +14,14 @@ public class BillRecordService {
     @Autowired
     BillRecordRepository billRecordRepository;
 
-    public boolean insertBillRecord(BillRecords billRecords){
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public void insertBillRecord(BillRecords billRecords){
+        int customerId = billRecords.getCustomer().getCustomerId();
+        Customer customer = customerRepository.findById(customerId).orElseThrow(() -> new RuntimeException("Customer not found"));
+        billRecords.setCustomer(customer);
         billRecordRepository.save(billRecords);
-        return true;
     }
 
     public List<BillRecords> getAllBillRecords(){
